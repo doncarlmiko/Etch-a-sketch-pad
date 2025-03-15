3786bc27949188f5d4b5152282919bc66f91a780
doncarlmiko <konoha11061997@gmail.com>
2025-03-15 22:02:14 +0800

    Added styling using javascript to adjust the cell size of the columns.

--- js/index.js
+++ js/index.js
@@ -2,21 +2,40 @@ const container=document.querySelector('.content-container');
 
 let counter;
 let counter2
-    for(counter=0; counter<16; counter++){
+
+
+const gridSize = 60;
+
+function createGrid(size){
+    //const totalCells = size * size;
+    container.textContent="";
+    
+    // Adjust cell size
+    const cellWidthSize = 500 / size; 
+    const cellHeightSize = 500/size;
+
+    for(counter=0; counter<size; counter++){
         const divRows=document.createElement('div');
         
-        for(counter2=0; counter2<16; counter2++){
+        for(counter2=0; counter2<size; counter2++){
             const divColumns=document.createElement('div');
+            
             divRows.appendChild(divColumns);
             divColumns.classList.add('columns');
+            divColumns.style.width=`${cellWidthSize}px`;
+            divColumns.style.height=`${cellHeightSize}px`;
 
             //Hovers each columns and change its color.
             divColumns.addEventListener("mouseover",(colorColumn)=>{
                 colorColumn.target.style.backgroundColor="red";
-            })
+            });
         }
         container.appendChild(divRows);
         divRows.classList.add('rows');
 
     }
+}
+
+createGrid(gridSize);
+    
 
