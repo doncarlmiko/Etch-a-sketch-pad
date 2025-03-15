4653010d1bc11049a537e8bd4b5b8aaa6ac0e308
doncarlmiko <konoha11061997@gmail.com>
2025-03-15 18:13:05 +0800

    Created an event to change the color of columns everytime the mouse hovers to it.

--- js/index.js
+++ js/index.js
@@ -2,14 +2,21 @@ const container=document.querySelector('.content-container');
 
 let counter;
 let counter2
-    for(counter=0; counter<10; counter++){
+    for(counter=0; counter<16; counter++){
         const divRows=document.createElement('div');
         
-        for(counter2=0; counter2<4; counter2++){
+        for(counter2=0; counter2<16; counter2++){
             const divColumns=document.createElement('div');
             divRows.appendChild(divColumns);
             divColumns.classList.add('columns');
+
+            //Hovers each columns and change its color.
+            divColumns.addEventListener("mouseover",(colorColumn)=>{
+                colorColumn.target.style.backgroundColor="red";
+            })
         }
         container.appendChild(divRows);
         divRows.classList.add('rows');
+
     }
+
